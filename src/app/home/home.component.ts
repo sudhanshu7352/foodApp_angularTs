1b1afaabaf052de2e6aac8ae780c4b87538dcb07
sudhanshu kumar <sudhanshuk384@gmail.com>
2023-05-02 15:31:30 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component,OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
+import { Foods } from '../shared/models/food';
+import {StarRatingComponent} from 'ng-starrating';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  foods:Foods[]=[];
+  constructor(private fs:FoodService){
+
+  }
+  ngOnInit(): void{
+    this.foods=this.fs.getAll();
+  }
+
+}
---
+++ src/app/services/food/food.service.ts
@@ -0,0 +1,102 @@
+import { Injectable } from '@angular/core';
+import { Foods } from 'src/app/shared/models/food';
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+
+  getAll():Foods[]{
+    return [
+     {
+       id:1,
+       name:'Butter Chicken',
+       price:5,
+       cookTime:'55-60',
+       favorite:true,
+       origins:['India','UK','USA'],
+       star:4.0,
+       imageUrl: '/assets/food1.jpg',
+       tags:['meal','Dinner']
+     },
+     {
+      id:2,
+      name:'Butter Paneer with Naan',
+      price:18,
+      cookTime:'35-40',
+      favorite:true,
+      origins:['India','Australia'],
+      star:4.8,
+      imageUrl: '/assets/food2.jpg',
+      tags:['meal','Dinner']
+    },
+    {
+      id:3,
+      name:'Pani Puri',
+      price:5,
+      cookTime:'15-20',
+      favorite:true,
+      origins:['India'],
+      star:4.6,
+      imageUrl: '/assets/food3.jpg',
+      tags:['meal','Dinner']
+    },
+    {
+      id:4,
+      name:'Fish fry',
+      price:15,
+      cookTime:'35-40',
+      favorite:true,
+      origins:['france','china','uk'],
+      star:4.3,
+      imageUrl: '/assets/food4.jpg',
+      tags:['meal','Dinner']
+    },
+    {
+      id:5,
+      name:'Fried rice',
+      price:6,
+      cookTime:'35-40',
+      favorite:true,
+      origins:['India','UK'],
+      star:3.5,
+      imageUrl: '/assets/food5.jpg',
+      tags:['meal','Dinner']
+    },
+    {
+      id:6,
+      name:'Chicken gravy',
+      price:14,
+      cookTime:'15-20',
+      favorite:true,
+      origins:['India','Bangladesh','uk'],
+      star:4.5,
+      imageUrl: '/assets/food6.jpg',
+      tags:['fastFood','Snack']
+    },
+    {
+      id:7,
+      name:'Dal fry',
+      price:9,
+      cookTime:'35-40',
+      favorite:true,
+      origins:['India','UK'],
+      star:4.0,
+      imageUrl: '/assets/food7.jpg',
+      tags:['meal','Dinner']
+    },
+    {
+      id:8,
+      name:'idli sambhar',
+      price:5,
+      cookTime:'25-30',
+      favorite:true,
+      origins:['India','sri lanka'],
+      star:3.8,
+      imageUrl: '/assets/food8.jpg',
+      tags:['meal','Dinner']
+    }
+    ]
+  }
+}
